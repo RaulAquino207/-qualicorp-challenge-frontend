b4959f9aee23a9658c40d5198c09e3a383585518
Raul Aquino <aquinoraul207@gmail.com>
2022-05-13 15:40:12 -0300

    table change to have action buttons

---
+++ src/components/CustomerModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="modal-container">
+    <h1>{{ show }}</h1>
+    <!-- <div class="modal">
+        <form>
+          <label for="m-nome">Nome</label>
+          <input id="m-nome" type="text" required />
+  
+          <label for="m-funcao">Função</label>
+          <input id="m-funcao" type="text" required />
+  
+          <label for="m-salario">Salário</label>
+          <input id="m-salario" type="number" required />
+          <button id="btnSalvar">Salvar</button>
+        </form>
+      </div> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "CustomerModal",
+  props: {
+    show: Boolean,
+  },
+};
+</script>
